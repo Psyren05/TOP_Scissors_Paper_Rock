c7e01d6d83548f2ede02f03609c335716042de8d
Lishan <lishan.low11@gmail.com>
2023-08-01 12:18:45 +0100

    added html

---
+++ main.js
@@ -0,0 +1,9 @@
+/* Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
+We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!*/
+
+function getComputerChoice() {
+    return Math.floor(Math.random * 3)
+}
+
+function getComputerChoice()
+
