b88fbc1f0cdc848c6d6b9b109eb292fb2a84573b
Lishan <lishan.low11@gmail.com>
2023-08-06 23:23:45 +0100

    revised game function

--- main.js
+++ main.js
@@ -5,31 +5,45 @@ function getComputerChoice () {
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === 'rock' && computerSelection === 'Paper') {
-      return "You Lose! Paper beats Rock"
-  } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
-      return "You Win! Scissors beats Paper"
-  } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
-      return "You Lose! Scissors beats Rock"
-  } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
-      return "You Win! Paper beats Rock"
+  if ((playerSelection === 'scissors' && computerSelection === 'Paper') || (playerSelection === 'paper' && computerSelection === 'Rock') || (playerSelection === 'rock' && computerSelection === 'Scissors')) {
+      return `"You Win! ${playerSelection} beats ${computerSelection}."`
   } else if (playerSelection === computerSelection.toLowerCase()) {
-      return "Draw!"
+      return "It's a tie!"
   } else {
       return "You Lose!"
   }
 }
 
+const playerSelection = prompt('Please enter your selection: Scissors, Paper or Rock')
+const computerSelection = getComputerChoice()
+const result = playRound(playerSelection, computerSelection)
+
 function game() {
   let playerScore = 0;
   let compScore = 0;
   
-for (let round = 1; round >= 5; round++) {
-  const playerSelection = prompt(`Round  ${round}.'Please enter your selection: Scissors, Paper or Rock'`);
-}
+  if (result === "`You Win! ${playerSelection} beats ${computerSelection}.`") {
+      playerScore++
+  } else if (result === "You Lose!") {
+      compScore++
+  }
+  
+  if (playerScore > compScore) {
+      console.log('Congratulations! You Win!')
+  } else if (compScore > playerScore) {
+      console.log('You Lost!')
+  } else {
+      console.log("It's a tie!")
+  }
+  
+  console.log(`'Final Score: You: ${playerScore}, Computer: ${compScore}'`)
 }
 
-console.log(prompt)
+game()
+
+
+
+
 
 
 
